916622f55813bb6e9bd9bb97681fd98cf87fde80
=meltus-bwire <meltusbwire10@gmail.com>
2025-11-09 23:12:34 +0300

    chore: remove the stripe model

--- tinker/gateways/stripe.py
+++ tinker/gateways/stripe.py
@@ -1,6 +1,6 @@
 from typing import Dict, Any
 from tinker.core.http import PaymentClient
-from tinker.core.schemas.payment import StripePaymentRequest, PaymentQueryResponse, PaymentResponse
+from tinker.core.schemas.payment import CardPaymentRequest, PaymentQueryResponse, PaymentResponse
 
 
 class Stripe(PaymentClient):
@@ -8,7 +8,7 @@ class Stripe(PaymentClient):
     def __init__(self, api_key: str | None = None, api_secret: str | None = None, base_url: str | None = None) -> None:
         super().__init__(api_key =api_key, api_secret =api_secret, base_url =base_url)
     
-    def initiate(self, data: StripePaymentRequest) -> PaymentResponse:
+    def initiate(self, data: CardPaymentRequest) -> PaymentResponse:
         payload: Dict[str, Any] ={ **data.model_dump(), 'gateway': 'stripe'}
         return super().initiate(payload)
 
