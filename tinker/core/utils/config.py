0156fb569306ed63180c6ee24eef4ef2b02f0920
=meltus-bwire <meltusbwire10@gmail.com>
2025-11-15 10:55:05 +0300

    chore: configred the base url for sdk & release envs

--- tinker/core/utils/config.py
+++ tinker/core/utils/config.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ENV = os.getenv("PYTHON_ENV")
+BASE_URL = os.getenv("BASE_URL", "")
+TINKER_BASE_URL ="https://payments.tinker.co.ke"
+is_prod_env = (ENV != "sdk_env") and (BASE_URL == "") 
+
+
 class Config:
     API_KEY = os.getenv("TINKER_API_KEY")
-    BASE_URL = os.getenv("TINKER_BASE_URL")
     API_SECRET = os.getenv("TINKER_API_SECRET")
+    BASE_URL = TINKER_BASE_URL if is_prod_env else BASE_URL
